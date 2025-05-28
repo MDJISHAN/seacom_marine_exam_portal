6d3b48f83aced7c8fdf362d1ad65f7e483ed3a8e
MDJISHAN <103403364+MDJISHAN@users.noreply.github.com>
2025-05-28 13:02:16 +0530

    Update apps.py

--- Exam/resultprocessing/apps.py
+++ Exam/resultprocessing/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ResultprocessingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resultprocessing'
+    name = 'Exam.resultprocessing'
