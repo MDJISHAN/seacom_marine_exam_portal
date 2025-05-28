f7be8ac3a54c88c110621d5a125144c541c090f7
MDJISHAN <103403364+MDJISHAN@users.noreply.github.com>
2025-05-28 15:14:21 +0530

    Update settings.py

--- Exam/examProject/settings.py
+++ Exam/examProject/settings.py
@@ -26,8 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Exam.student',
-
-    'Exam.course',
     'Exam.resultprocessing',
     'Exam.studentPreferences',
     'Exam.questions',
