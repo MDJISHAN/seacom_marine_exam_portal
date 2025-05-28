6e79dd445c67479a389460dd1712400cb1524bda
MDJISHAN <103403364+MDJISHAN@users.noreply.github.com>
2025-05-28 16:38:42 +0530

    Update settings.py

--- Exam/examProject/settings.py
+++ Exam/examProject/settings.py
@@ -142,11 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'Exam.examProject/static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Exam', 'static')]  # optional, for dev
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where files will be collected
+
 
 MESSAGE_TAGS = {
     messages.ERROR : 'danger',
