81a54635a0b4dc98182fff52b0c93965e645e33e
MDJISHAN <103403364+MDJISHAN@users.noreply.github.com>
2025-05-28 15:06:32 +0530

    Update models.py

--- Exam/tuition/models.py
+++ Exam/tuition/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from Exam.admission.models import *
+from admission.models import *
 # from student.models import *
 
 # Create your models here.
